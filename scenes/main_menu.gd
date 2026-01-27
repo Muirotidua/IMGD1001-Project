71c656e261198bc9966ed82b2e976c7e356f62e4
Muirotidua <liamblackwel@gmail.com>
2026-01-26 20:05:38 -0500

    Sorry last thing was broken just fixed it.

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -1,8 +1,5 @@
 extends Node2D
 
-@onready var startSelect = $"Start Select" # start select plays when player starts game. - EH
-@onready var optionSelect = $"Option Select" # option select plays when player selects any other button. A better name is possible. - EH
-
 var clicked = null;
 var loaded = false;
 
@@ -11,28 +8,6 @@ func _ready() -> void:
 	$Fade/AnimationPlayer.play("fade_out_black")
 
 func _on_start_pressed() -> void:
-<<<<<<< Updated upstream
-	clicked = "start";
-	$"Fade/Fade Timer".start()
-	$Fade/AnimationPlayer.play("fade_in_black")
-	startSelect.play() #This gets cut off by scene transition. Could rework to be shorter than fade, but an option that doesn't require that is preferable - EH
-	
-
-func _on_version_notes_pressed() -> void:
-	clicked = "version_notes";
-	$Fade.show()
-	$"Fade/Fade Timer".start()
-	$Fade/AnimationPlayer.play("fade_in_black")
-	optionSelect.play()
-
-
-func _on_credits_pressed() -> void:
-	clicked = "credits";
-	$Fade.show()
-	$"Fade/Fade Timer".start()
-	$Fade/AnimationPlayer.play("fade_in_black")
-	optionSelect.play()
-=======
 	if clicked == null:
 		clicked = "start";
 		$"Fade/Fade Timer".start()
@@ -53,12 +28,10 @@ func _on_credits_pressed() -> void:
 		$Fade.show()
 		$"Fade/Fade Timer".start()
 		$Fade/AnimationPlayer.play("fade_in_black")
->>>>>>> Stashed changes
 
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
-	optionSelect.play()
 	
 
 func _on_fade_timer_timeout() -> void:
