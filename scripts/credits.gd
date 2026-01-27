ca5b1b318a4e2992437ec3cb4695d87a4934bd32
Muirotidua <liamblackwel@gmail.com>
2026-01-27 18:12:58 -0500

    Fix the main_menu corruption

--- scripts/credits.gd
+++ scripts/credits.gd
@@ -6,4 +6,3 @@ func _on_back_pressed() -> void:
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if anim_name == "Credit_Scroll":
 		$CreditsText.scroll_active = true
-	
