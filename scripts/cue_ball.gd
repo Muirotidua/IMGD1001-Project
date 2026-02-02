310f702d024c97f5d57157e47d03986883b39702
Eric F <ejfarquharson@wpi.edu>
2026-02-02 01:56:04 -0500

    Added a pointer line
    -Added a pointer line to the ball, which will be improved
    - changed start scene to level_template for testing

--- scripts/cue_ball.gd
+++ scripts/cue_ball.gd
@@ -4,8 +4,28 @@ extends BaseBall
 @export var dls = 1000
 @export var infinite_shots: bool = false
 
+@onready var pointer = $PointerLine
+
+func _ready():
+	pointer.add_point(Vector2.ZERO)
+	pointer.add_point(get_local_mouse_position())
 func _input(event):
 	if(event.is_action_pressed("ball_hit")):
 		var direction = global_position.direction_to(get_global_mouse_position())
 		if !inmotion || infinite_shots:
 			apply_central_impulse(direction * dls)
+			
+
+func _process(delta: float) -> void:
+	pointer.set_point_position(1, get_local_mouse_position())
+	
+	if inmotion:
+		pointer.hide()
+	
+	if !inmotion:
+		pointer.show()
+		
+		
+	
+	
+	
