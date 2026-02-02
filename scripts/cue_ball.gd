b5b259f4b2a828485dc4d67980ba4bd5c993c627
Muirotidua <liamblackwel@gmail.com>
2026-02-01 19:28:03 -0500

    Added Table Borders / Pockets
    Very basic stuff, temporary to be sure

--- scripts/cue_ball.gd
+++ scripts/cue_ball.gd
@@ -1,25 +1,11 @@
-extends RigidBody2D
+extends BaseBall
 
-@onready var speed_label: Label = $Label
-@export var dls = 1000 # Default Launch Speed
-var inmotion: bool = false
-var speed
+# Default Launch Speed
+@export var dls = 1000
+@export var infinite_shots: bool = false
 
 func _input(event):
 	if(event.is_action_pressed("ball_hit")):
-		var direction = position.direction_to(get_global_mouse_position())
-		if !inmotion:
-			apply_central_impulse(direction*dls)
-	if(event.is_action_pressed("move_left")):
-		pass
-
-func _physics_process(delta: float) -> void:
-	speed = abs(linear_velocity.length())
-	speed_label.text = str(speed)
-	if speed < 10:
-		linear_damp = 50
-		if speed < 1:
-			linear_damp = 1
-			inmotion = false
-	else:
-		inmotion = true
+		var direction = global_position.direction_to(get_global_mouse_position())
+		if !inmotion || infinite_shots:
+			apply_central_impulse(direction * dls)
