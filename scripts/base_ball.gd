2909f251a06fb02c24a12a821baaf0bcb48d1845
wun2tm <139637148+wun2tm@users.noreply.github.com>
2026-02-05 12:52:47 -0500

    art for cue ball,8 ball, and cue stick

--- scripts/base_ball.gd
+++ scripts/base_ball.gd
@@ -5,7 +5,7 @@ class_name BaseBall extends RigidBody2D
 
 @onready var motion_label: Label = $NonRotate/MotionLabel
 @onready var speed_label: Label = $NonRotate/SpeedLabel
-@onready var sprite: Sprite2D = get_node(sprite_path)
+@onready var sprite: AnimatedSprite2D = get_node(sprite_path)
 
 const DEFAULT_SCALE: Vector2 = Vector2(0.075, 0.075)
 
