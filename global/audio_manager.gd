46d3bbd3f5bf672883c70e6569b07e93ee500480
ewhanford <ewhanford@wpi.edu>
2026-02-24 21:49:14 -0500

    Music!
    From Eric.

--- global/audio_manager.gd
+++ global/audio_manager.gd
@@ -2,6 +2,14 @@ extends Node
 
 
 @onready var music: AudioStreamPlayer = $Music
+var fail = preload("res://sounds/sfx/level-fail.wav")
+var onestar = preload("res://sounds/sfx/level-one-star.wav")
+var twostar = preload("res://sounds/sfx/level-two-star.wav")
+var threestar = preload("res://sounds/sfx/level-three-star.wav")
+var completesounds = [fail,onestar,twostar,threestar]
+
+var rng = RandomNumberGenerator.new()
+var ballsinksounds = [preload("res://sounds/sfx/ball-sink-1.wav"),preload("res://sounds/sfx/ball-sink-2.wav"),preload("res://sounds/sfx/ball-sink-3.wav")]
 
 # having all of these as helper functions should make the in-script calls less complex, 
 # and allow for intermediate steps between the call being made and playing the audio
@@ -15,14 +23,16 @@ func ball_break():
 func level_complete(_type: int): # based on number, hopefully can be set in script and be a single level complete call.
 								 # should be 0 = fail, 1-3 = that many stars
 	print(_type)
+	$LevelComplete.stream = completesounds[_type]
 	$LevelComplete.play()
 
 func wall_hit():
-	print("wall hit")
 	$WallHit.play()
 
 func stick_hit():
 	$StickHit.play()
 
 func ball_sink(): #should randomize between the three available takes ideally. Important for this sound b/c it's crunchy
+	var soundnum: int = rng.randi_range(0,2)
+	$BallSink.stream = ballsinksounds[soundnum]
 	$BallSink.play()
