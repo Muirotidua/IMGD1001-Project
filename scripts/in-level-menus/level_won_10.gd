8ae28c1cf88ef4037fc78cf9d73d3ef7c6c2333d
Muirotidua <liamblackwel@gmail.com>
2026-02-28 17:59:42 -0500

    Fixed level 10 victory

--- scripts/in-level-menus/level_won_10.gd
+++ scripts/in-level-menus/level_won_10.gd
@@ -2,5 +2,8 @@ extends Node2D
 
 @onready var current_level = get_tree().current_scene
 
+signal restart()
+signal next_lev()
+
 func _on_continue_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/info-pages/credits.tscn")
