12ebabd8d0f5c03118935344ccb2e5118953796f
Eric F <ejfarquharson@wpi.edu>
2026-03-04 16:08:39 -0500

    Audio Fix
    Fixed and Everything should be good!

--- audio_levels.gd
+++ audio_levels.gd
@@ -5,8 +5,9 @@ var MusicLevel: float = 0.5
 var SFXLevel: float = 0.5
 var VoiceLevel: float = 0.5
 
-func set_audio(Master: float, Music: float , SFX: float):
+func set_audio(Master: float, Music: float , SFX: float, Voice: float):
 	MasterLevel = Master
 	MusicLevel = Music
 	SFXLevel = SFX
+	VoiceLevel = Voice
 	
