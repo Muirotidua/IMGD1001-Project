6ea7ef3ba3de3c7e2be4424529e0f40923a12598
Eric F <ejfarquharson@wpi.edu>
2026-03-04 17:23:22 -0500

    One last time i think
    -Credits bug

--- scripts/info-pages/credits.gd
+++ scripts/info-pages/credits.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var first = true
+var early = false
 
 func _ready():
 	$"Fade/Fade Timer".start()
@@ -9,10 +10,12 @@ func _ready():
 	$AnimationPlayer.play("Credit_Scroll")
 
 func _on_back_pressed() -> void:
+	early = true
 	_on_animation_player_animation_finished("Credit_Scroll")
 
 func _on_animation_player_animation_finished(_anim_name: StringName) -> void:
-	await get_tree().create_timer(3).timeout
+	if !(early):
+		await get_tree().create_timer(3).timeout
 	$"Fade/Fade Timer".start()
 	$Fade.show()
 	$Fade/AnimationPlayer.play("fade_in_black")
